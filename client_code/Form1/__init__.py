718337a444651447a9b88ae28bcc45060b6ed387
wirob@protonmail.com <wirob@protonmail.com>
2024-01-18 17:43:20 +0000

    Created ServerModule1 and icons8-partly-cloudy-day-96.png; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,10 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 import plotly.graph_objects as go
+from anvil.js.window import navigator
+
+Plot.templates.default = "rally"
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -8,3 +12,36 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    
+    # Initialize location to Cambridge, UK
+    self.set_location(52.205, 0.1218)  
+
+    #print the output from the OpenWeatherMap API
+    print(anvil.server.call('get_forecast_data', self.location))
+
+  
+  def set_location(self, lat, lon):
+    self.location = {"lat": lat, "lon": lon}
+    self.location_label.text = f"Showing weather forecast for {lat}, {lon}."
+    self.refresh()
+
+  
+  def refresh(self):
+    times, temps, summary_data = anvil.server.call('get_forecast_data', self.location)
+    self.plot_1.data = go.Scatter(x=times, y=temps)
+    self.plot_1.layout.title = "5-day Forecast" 
+    
+    self.temp_label.text = f"{temps[0]}°C"
+    self.min_label.text = f"L:{summary_data['temp_min']}°C"
+    self.max_label.text = f"H:{summary_data['temp_max']}°C"
+    self.description_label.text = summary_data['description'].capitalize()
+    self.icon.source = f"https://openweathermap.org/img/wn/{summary_data['icon']}@4x.png"
+
+  def timer_1_tick(self, **event_args):
+    """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+    self.refresh()
+
+  def location_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    navigator.geolocation.getCurrentPosition(lambda p: self.set_location(p.coords.latitude, p.coords.longitude))
+    self.location_button.enabled = False
